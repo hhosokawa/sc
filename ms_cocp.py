3c947199629bbf08f39464a7dfc7be1b1a2a7be2
Hiro <hiro.hosokawa@gmail.com>
2013-02-04 14:32:58 -0500

    31-jan-13 ms cocp update

--- ms_cocp.py
+++ ms_cocp.py
@@ -2,12 +2,13 @@ import csv
 import time
 from collections import defaultdict
 
-output = 'o/2013 Jan 15 - NNEA COCP Summary.csv'
-input1 = 'i/ms_cocp/COCP Summary to Jan 15 2013.csv'
-input2 = 'i/ms_cocp/NNEA Summary to Jan 15 2013.csv'
+output = 'o/2013 Jan 31 - NNEA COCP Summary.csv'
+input1 = 'i/ms_cocp/COCP Summary to Jan 31 2013.csv'
+input2 = 'i/ms_cocp/NNEA Summary to Jan 31 2013.csv'
 
 #################################################################################
 ## Function Definition
+
 def csv_dic(filename, style=1):     # Converts CSV to dict
     reader = csv.reader(open(filename, "rb"))
     if style == 1: my_dict = dict((k, v) for k, v in reader)
@@ -24,8 +25,7 @@ def header_add(header):             # Adds New Headers
 def clean(r, datatype):
     if datatype == 'COCPEnrol':
         r['Acct Type'] = 'Enrollment'
-        try: r['Contract Program Name'] = enrolprogram[r['Enrollment #']]
-        except: r['Contract Program Name'] = ''
+        r['Contract Program Name'] = enrolprogram.get(r['Enrollment #'], '')
         if r['Type'] == 'Win': r['COCP Win'] = r['Enrollment #']
         else: r['COCP Loss'] = r['Enrollment #']
     elif datatype == 'COCPAcct':
@@ -35,27 +35,27 @@ def clean(r, datatype):
         else: r['COCP Loss'] = r['Master #']
     elif datatype == 'NNEAEnrol':
         r['Acct Type'] = 'Enrollment'
-        if r['Contract Category'] == 'New Contract':
-            r['NNEA'] = r['Contract Number']
+        if r['Contract Category'] == 'New Contract': r['NNEA'] = r['Contract Number']
         else: r['Renewal'] = r['Contract Number']
     elif datatype == 'NNEAAcct':
         r['Acct Type'] = 'Acct'
         r['Contract Program Name'] = ''
-        if r['Contract Category'] == 'New Contract':
-            r['NNEA'] = r['Master Number']
+        if r['Contract Category'] == 'New Contract': r['NNEA'] = r['Master Number']
         else: r['Renewal'] = r['Master Number']
     r['OB Rep'] = r['OB Rep'].strip()
     r['Customer'] = r['Customer'].upper()
     try: r['Rep Type'] = titleOBTSR[emp_stb[r['OB Rep']][1]]
-    except: r['Rep Type'] = 'N/A'
+    except KeyError: r['Rep Type'] = 'N/A'
     try: r['Branch'] = emp_stb[r['OB Rep']][0]
-    except: r['Branch'] = 'N/A'
+    except KeyError: r['Branch'] = 'N/A'
     return r
 
-
-# Requires Updating
-enrolprogram = csv_dic('auxiliary\\enrol-program.csv')
+"""
+#################################################################################
+## Requires Updating
+"""
 emp_stb = csv_dic('auxiliary\\employee-super_title_branch.csv', 3)
+enrolprogram = csv_dic('auxiliary\\enrol-program.csv')
 
 # Pictionary Jars
 divregion = csv_dic('auxiliary\\div-region.csv')
@@ -66,23 +66,32 @@ enrolset, accttypeset, masterset = set(), set(), set()
 ## Main
 def main():
     t0 = time.clock()
-    header = set()                  # All input headers -> output header
+
+    # All input headers -> output header
+    header = set()                  
     with open(input1) as i1: header.update(csv.DictReader(i1).fieldnames)
     with open(input2) as i2: header.update(csv.DictReader(i2).fieldnames)
     header = header_add(header)
     header = tuple(header)
 
-    with open(output, 'wb') as o:   # Analyze Input 1-2 -> Output
+    # Analyze Input 1-2 -> Output
+    with open(output, 'wb') as o:   
         writer = csv.writer(o)
         ow = csv.DictWriter(o, fieldnames=header)
         writer.writerows([header])
         with open(input1) as i1:
             i1r = csv.DictReader(i1)
-            for r in i1r:           # COCP Enrollment Cleaning
+            
+            # COCP Enrollment Cleaning
+            for r in i1r:           
                 enrolset.add(r['Enrollment #'])
                 r = clean(r, 'COCPEnrol')
+                if 'Region' in r['Region']:
+                    r['Region'] = r['Region'].replace('Region', '')
+                    r['Region'] = r['Region'].strip()
                 ow.writerow(r)
-                                    # COCP Acct Cleaning
+                                    
+                # COCP Acct Cleaning
                 r['COCP Loss'], r['COCP Win'] = '', ''
                 acctid = r['Master #'] + r['Type']
                 if acctid not in accttypeset:
@@ -92,22 +101,27 @@ def main():
 
         with open(input2) as i2:
             i2r = csv.DictReader(i2)
-            for r in i2r:           # NNEA Enrollment Cleaning
+
+            # NNEA Enrollment Cleaning
+            for r in i2r:           
                 r['OB Rep'] = r['Master OB Rep Name']
                 r['Master #'] = r['Master Number']
                 r['Customer'] = r['Master Name']
                 masterdivloc = r['Master Division'] + r['Master Divloc']
                 r['Region'] = divregion[masterdivloc]
-                if r['Region'] == 'Canada': r['Region'] = 'Canada Region'
-                if masterdivloc == '10099':
-                    r['Region'] = 'US Corporate'
-                elif masterdivloc == '20099':
-                    r['Region'] = 'Canada Region'
+                if r['Region'] == 'Canada': r['Region'] = 'Canada'
+                if masterdivloc == '10099': r['Region'] = 'US Corporate'
+                elif masterdivloc == '20099': r['Region'] = 'Canada'
+                if 'Region' in r['Region']:
+                    r['Region'] = r['Region'].replace('Region', '')
+                    r['Region'] = r['Region'].strip()
+            
                 if r['Contract Number'] not in enrolset:
                     enrolset.add(r['Contract Number'])
                     r = clean(r, 'NNEAEnrol')
                     ow.writerow(r)
-                                    # NNEA Acct Cleaning
+                                    
+                # NNEA Acct Cleaning
                 r['NNEA'], r['Renewal'] = '', ''
                 if r['Master Number'] not in masterset:
                     masterset.add(r['Master Number'])
