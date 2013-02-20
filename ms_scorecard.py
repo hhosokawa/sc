b8809a4c9e4af271dd124b347983e9b638a4e1bf
Hiro <hiro.hosokawa@gmail.com>
2013-02-20 17:27:44 -0500

    add true-up data analysis

--- ms_scorecard.py
+++ ms_scorecard.py
@@ -1,19 +1,20 @@
 import csv
 import time
-from decimal import Decimal
+from decimal import Decimal as d
 from datetime import datetime, timedelta
 import dateutil.parser as dparser
 from aux_reader import *
 
-output = 'o\\04-Jan-13 Enrol Repo.csv'
+output = 'o\\20-Feb-13 Enrol Repo.csv'
 input1 = 'i\\ms_scorecard\\referrals.csv'
 input2 = 'i\\ms_scorecard\\contract repo.csv'
-currentyear = datetime(2012, 1, 1).date()
-currentyearend = datetime(2013, 1, 1).date()
+currentyear = datetime(2013, 1, 1).date()
+currentyearend = datetime(2014, 1, 1).date()
 
-d = Decimal
 growth = tree()
 renew = tree()
+trueup = tree()
+enroltu = set()
 enrolgp = tree()
 divregion = csv_dic('auxiliary\\div-region.csv')
 divdistrict = csv_dic('auxiliary\\div-district.csv')
@@ -30,7 +31,15 @@ def annualbill(r):
     else:
         return False
 
-# EA Growth - Datascrape: Referrals -> EA Growth Data
+# Determines if row is a True-Up
+def trueupcheck(r):
+    if (r['Referral Rev Type'] == 'TU' 
+    or r['Product Item Bill Type'] == 'TRUP'):
+        return True
+    else:
+        return False
+
+# Datascrape: Referrals -> EA Growth Data
 def growth_scrape(r):
     enrol = r['Contract Enrollment']
     startdate = dparser.parse(r['Enrollment Start Date']).date()
@@ -58,33 +67,62 @@ def growth_scrape(r):
     if annualbill(r) and d(r['Referral Net Expected Fee Total']) > 0:
         gp = d(r['Referral Net Expected Fee Total']) + d(r['Referral Partner Fee'])
         enrolgp[enrol] = gp
+
+    # Absorb Enrol:True-Up Dict
+    if (trueupcheck(r) 
+    and int(r['Referral Fiscal Year']) == int(currentyear.strftime("%Y"))-1):
+        enroltu.add(enrol)
     return
 
-# Renewal Analysis - Datascrape: Contract Repo -> Renewal Analysis
-def renew_scrape(r):
+# Remap Contract Repo -> Dict Headers
+def remap(r, style):
     enrol = r['Contract Number']
     startdate = dparser.parse(r['Contract Start Date']).date()
     enddate = dparser.parse(r['Contract End Date']).date()
-    if enrol not in renew and currentyear <= enddate < currentyearend:
-        x = renew[enrol] # id reference
-        x['Enrol #'] = enrol
-        x['Cust Name'] = r['Master Name']
-        x['Cust Class'] = r['Contract Level']
-        x['Start Date'] = startdate.strftime("%Y-%m")
-        x['End Date'] = enddate.strftime("%Y-%m")
+    
+    # Identify Renewal / True-Up Executed
+    if style == 'renew': 
+        x = renew[enrol]
+        reporttype = 'Renewal Analysis'
         x['Period'] = int(enddate.strftime("%m"))
-        divloc = r['Master Division'] + r['Master Divloc']
-        x['GP'] = enrolgp.get(r['Renewed to Vendor Contract ID'], 0)
-        x['Region'] = divregion.get(divloc, '')
-        x['District'] = divdistrict.get(divloc, '')
-        x['Report Type'] = 'Renewal Analysis'
-        renewid = r['Renewed to Vendor Contract ID']
-        if renewid == '': 
-            x['Custom Category A'] = 'Not Renewed'
+        if r['Renewed to Vendor Contract ID'] <> '': 
+            x['Custom Category A'] = 'A) Renewed'
+            x['GP'] = enrolgp.get(r['Renewed to Vendor Contract ID'], 0)
         else: 
-            x['Custom Category A'] = 'Renewed'
+            x['Custom Category A'] = 'B) Not Renewed'
+    elif style == 'trueup':
+        x = trueup[enrol]
+        reporttype = 'True-Up Analysis'
+        x['Period'] = int(startdate.strftime("%m"))
+        if enrol in enroltu:
+            x['Custom Category A'] = 'A) Executed'
+        else:
+            x['Custom Category A'] = 'B) Not Executed'
+
+    x['Enrol #'] = enrol
+    x['Cust Name'] = r['Master Name']
+    x['Cust Class'] = r['Contract Level']
+    x['Start Date'] = startdate.strftime("%Y-%m")
+    x['End Date'] = enddate.strftime("%Y-%m")
+    divloc = r['Master Division'] + r['Master Divloc']
+    x['Region'] = divregion.get(divloc, '')
+    x['District'] = divdistrict.get(divloc, '')
+    x['Report Type'] = reporttype
+    return 
+
+# Datascrape: Contract Repo -> Renewal/True-Up Analysis
+def renew_scrape(r):
+    enrol = r['Contract Number']
+    enddate = dparser.parse(r['Contract End Date']).date()
+
+    if (enrol not in renew 
+    and currentyear <= enddate < currentyearend): 
+        remap(r, 'renew')
+    if (enrol not in trueup 
+    and enddate >= currentyearend): 
+        remap(r, 'trueup')
     return
-        
+      
 #################################################################################
 ## Main
 
@@ -101,10 +139,10 @@ def main():
         for r in csv.DictReader(i1): growth_scrape(r)
         print 'EA Growth Analysis Complete.', time.clock()-t0
 
-    # Contract Repo Datascrape -> Renewal Analysis
+    # Contract Repo Datascrape -> Renewal /True Up Analysis
     with open(input2) as i2:
         for r in csv.DictReader(i2): renew_scrape(r)
-        print 'Renewal Analysis Complete.', time.clock()-t0
+        print 'Renewal / True-Up Analysis Complete.', time.clock()-t0
 
     # Write to Output
     with open(output, 'wb') as o:
@@ -113,7 +151,7 @@ def main():
         writer.writerows([header])
         for enr in growth: ow.writerow(growth[enr])
         for enr in renew: ow.writerow(renew[enr])
-
+        for enr in trueup: ow.writerow(trueup[enr])
     t1 = time.clock()
     return 'Process completed! Duration:', t1-t0
 
