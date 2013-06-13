44120df6d337c65ff8aaf24e66b701d3517fb249
Hiro <hiro.hosokawa@gmail.com>
2013-06-13 10:53:12 -0400

    pandas test

--- pandas_test.py
+++ pandas_test.py
@@ -1,43 +1,21 @@
 import pandas as pd
-import numpy as np
 
-data = pd.read_csv('c:/users/hhos/Downloads/fortune1000.csv')
-famous = {'3Com': 'Cool'}
-
-def revenuecheck(revenue):
-    if 0 < revenue < 1000:
-        return 'A'
-    elif 1001 < revenue < 2000:
-        return 'B'
-    elif 2001 < revenue < 3000:
-        return 'C'
-    else:
-        return 'Tosin Abasi'
-
-def revenuecheck2(rev):
-    if 0 < rev < 2000:
-        return 'Super 1'
-    elif 2001 < rev < 3000:
-        return 'Super 2'
-    elif 3001 < rev < 4000:
-        return 'Mega 3'
-    else:
-        return 'Mega Tosin Abasi'
+datachunks = pd.read_csv(
+             'C:/Portable Python 2.7/App/Scripts/i/database/2012-sales.csv',
+             iterator=True, chunksize=1000)
+data = pd.concat([chunk for chunk in datachunks], ignore_index=True)
 
 def main():
     print data.head(), '\n'
-    data['category'] = data['revenue'].map(revenuecheck)
-    data['category 2'] = data['revenue'].map(revenuecheck2)
 
-    print 'Edit 1'
-    print data.head(), '\n'
+if __name__ == '__main__':
+    main()
+
+
+"""
+    data['category'] = data['revenue'].map(revenuecheck)
 
     grouped = data.groupby(['category', 'category 2'])
     pivot = grouped.sum()
     print pivot.head()
-
-
-
-
-if __name__ == '__main__':
-    main()
+"""
