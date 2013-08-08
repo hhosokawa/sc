f64b23997384a58d611d487d52aa7e981629d0b6
Hiro <hiro.hosokawa@gmail.com>
2013-08-08 14:02:38 -0400

    Aug update cocp

--- ms_cocp.py
+++ ms_cocp.py
@@ -5,9 +5,9 @@ import dateutil.parser as dparser
 from datetime import datetime, timedelta, date
 from collections import defaultdict
 
-output = 'o/NNEA COCP Summary - 2013-07-17.csv'
-input1 = 'i/ms_cocp/COCP Summary to 2013-07-17.csv'
-input2 = 'i/ms_cocp/NNEA Summary to 2013-07-17.csv'
+output = 'o/NNEA COCP Summary - 2013-08-08.csv'
+input1 = 'i/ms_cocp/COCP Summary to 2013-08-08.csv'
+input2 = 'i/ms_cocp/NNEA Summary to 2013-08-08.csv'
 enrolprogram = csv_dic('auxiliary\\enrol-program.csv')
 emp_stb = csv_dic('auxiliary\\employee-super_title_branch.csv', 3)
 
