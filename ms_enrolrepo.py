d472ad4d4b4c6f5d0dd628154abd8aa08fc41828
Hiro <hiro.hosokawa@gmail.com>
2013-10-03 11:03:58 -0400

    add: district classification

--- ms_enrolrepo.py
+++ ms_enrolrepo.py
@@ -2,29 +2,35 @@ import time
 import pandas as pd
 from aux_reader import *
 
-output    = 'o\\Enrol Repo - 2013-07-04.csv'
-repo_df   = pd.read_csv('i\\enrol_repo\\contract_repo.csv')
-tsr_dict  = csv_dic('i\\enrol_repo\\tsr reps.csv')
+""" io """
 
-divregion = csv_dic('auxiliary\\div-region.csv')
+repo_df   =     pd.read_csv('i\\enrol_repo\\contract_repo.csv')
+tsr_dict  =     csv_dic('i\\enrol_repo\\tsr reps.csv')
+divregion =     csv_dic('auxiliary\\div-region.csv')
+divdistrict =   csv_dic('auxiliary\\div-district.csv')
 
-#################################################################################
-## Function Definitions
+output    =     'o\\Enrol Repo - %s.csv' % (time.strftime("%Y-%m-%d"))
+
+############### Uitls ###############
 
 def region(series):
     divloc = (str(int(series['Master Division'])) +
               str(int(series['Master Divloc'])))
     return divregion.get(divloc, '')
 
+def district(series):
+    divloc = (str(int(series['Master Division'])) +
+              str(int(series['Master Divloc'])))
+    return divdistrict.get(divloc, '')
+
 def OB_TSR(series):
     rep = str(int(series['Master OB Rep']))
     if rep in tsr_dict: return 'TSR'
     else: return 'OB'
 
-#################################################################################
-## Main
+############### enrol_repo_main() ###############
 
-def main():
+def enrol_repo_main():
     t0 = time.clock()
 
     # Fill Missing Values
@@ -38,6 +44,7 @@ def main():
 
     # Fill Region and TSR / OB Columns
     repo_df['Region'] = repo_df.apply(region, axis = 1)
+    repo_df['District'] = repo_df.apply(district, axis = 1)
     repo_df['OB / TSR'] = repo_df.apply(OB_TSR, axis = 1)
 
     # Drop Duplicates
@@ -46,6 +53,9 @@ def main():
     # Write to CSV
     repo_df.to_csv(output, index=False)
     t1 = time.clock()
-    return 'Process completed! Duration:', t1-t0
+    print 'enrol_repo_main() completed. Duration:', t1-t0
+    return
 
-print main()
+if __name__ == '__main__':
+    enrol_repo_main()
+
