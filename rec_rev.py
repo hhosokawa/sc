1fc660a936bede4cfa2b59ce5e5a021accd42c16
hhosokawa <hiro.hosokawa@gmail.com>
2014-01-04 17:25:19 -0500

    complete: rec_rev.py best scenario

--- rec_rev.py
+++ rec_rev.py
@@ -2,12 +2,12 @@ import csv
 import time
 from aux_reader import *
 
+input0 = 'i/rec_rev/2013.csv'
 output = 'o/rec_rev_best.csv'
-input0 = 'i/rec_rev/bi.csv'
 
 rev_rec_flag = ['SWBNDL', 'HWMTN', 'SWMTN', 'CLOUDN', 
                 'SWSUB', 'CLOUD', 'TRAIN']
-lic_valid_per_items = csv_dic('i/rec_rev/lic_valid_period_items.csv')
+#lic_valid_per_items = csv_dic('i/rec_rev/lic_valid_period_items.csv')
 
 ############### Utils ###############
 
@@ -32,7 +32,6 @@ def loop_row(i0, ow):
         gp = float(r['GP (includes Freight)'])
 
         if (r['Sale or Referral'] == 'Referral' or
-            r['Item Number'] in lic_valid_per_items or
             r['Revenue Recognition ID'] in rev_rec_flag or
             r['Super Category @ Order Date'] == 'Managed Services'):
             r['Item Class'] = 'Recurring Revenue'
@@ -40,7 +39,6 @@ def loop_row(i0, ow):
             r['Item Class'] = 'Project'
         else:
             r['Item Class'] = 'Run Rate'
-
         ow.writerow(r)
 
 ############### Main ###############
