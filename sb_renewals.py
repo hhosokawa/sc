9c15f6963bc45090437f5e0a8e85ad71c933526f
hhosokawa <hiro.hosokawa@gmail.com>
2014-03-12 15:31:47 -0400

    add: new fee matrix, ESA 3.0, 4.0

--- sb_renewals.py
+++ sb_renewals.py
@@ -12,7 +12,7 @@ fb_data = tree()
 current_year = 2014
 input0 = 'i/sb_renewals/contract_repo.csv'
 input1 = 'i/sb_renewals/futurebillings.csv'
-output = 'o/sb renewals - %s.csv' % (time.strftime("%Y-%m-%d"))
+output = 'P:/_HHOS/Microsoft/SB Renewals - %s.csv' % (time.strftime("%Y-%m-%d"))
 
 divregion = csv_dic('auxiliary/div-region.csv')
 divdistrict = csv_dic('auxiliary/div-district.csv')
@@ -38,7 +38,7 @@ def calc_gp(r):
     # Assign Contract Units
     if r['Contract Units']:
         units = int(r['Contract Units'])
-    else: units = 0
+    else: units = -1
 
     # Assign Imputed Revenue
     if r['Contract Payment Amount']:
@@ -50,13 +50,37 @@ def calc_gp(r):
     else:
         imputed_rev = 0
 
-    # Fee Matrix
-    if 0 <= units <= 749:           percent = 0.15
-    elif 750 <= units <= 2399:      percent = 0.12
-    elif 2400 <= units <= 5999:     percent = 0.1
-    elif 6000 <= units <= 14999:    percent = 0.015
-    elif units >= 15000:            percent = 0.0025
-    else:                           percent = 0.0775
+    # GP Fee Matrix
+    start_date = dparser.parse(r['Contract Payment Date'])
+    if dparser.parse('2013-09-30') < start_date:
+
+        # ESA 3.0 date < 2013-09-30
+        if 0 <= units <= 749:           percent = 0.06
+        elif 750 <= units <= 2399:      percent = 0.06
+        elif 2400 <= units <= 5999:     percent = 0.055
+        elif 6000 <= units <= 14999:    percent = 0.033
+        elif units >= 15000:            percent = 0.03
+        else:                           percent = 0.048
+    else:
+
+        # ESA 4.0 - Renewal
+        if r['Type'] == 'Renewal':
+            if 0 <= units <= 749:           percent = 0.085
+            elif 750 <= units <= 2399:      percent = 0.0488
+            elif 2400 <= units <= 5999:     percent = 0.0375
+            elif 6000 <= units <= 14999:    percent = 0.0113
+            elif units >= 15000:            percent = 0.0083
+            else:                           percent = 0.0382
+
+        # ESA 4.0 - Scheduled Billings
+        else:
+            if 0 <= units <= 749:           percent = 0.025
+            elif 750 <= units <= 2399:      percent = 0.01125
+            elif 2400 <= units <= 5999:     percent = 0.01
+            elif 6000 <= units <= 14999:    percent = 0.00625
+            elif units >= 15000:            percent = 0.0045
+            else:                           percent = 0.0114
+
     return imputed_rev * percent
 
 def get_region_district(r):
@@ -83,9 +107,6 @@ def scan_contract_repo():
                     if pay_date > pay_date2:
                         pass
                 else:
-                    r['GP'] = calc_gp(r)
-                    r['Imputed Rev'] = r['Contract Payment Amount']
-                    r['Region'], r['District'] = get_region_district(r)
                     if start <= end_date <= end:
                         r['Type'] = 'Renewal'
                         r['Fiscal Year'] = end_date.strftime('%Y')
@@ -96,7 +117,12 @@ def scan_contract_repo():
                         r['Fiscal Month'] = pay_date.strftime('%m')
                     else:
                         continue
+                    r['GP'] = calc_gp(r)
+                    r['Imputed Rev'] = r['Contract Payment Amount']
+                    r['Region'], r['District'] = get_region_district(r)
+
                     data[contract] = r
+
     print 'scan_contract_repo() completed.'
 
 def add_renewal_trueup():
@@ -104,7 +130,7 @@ def add_renewal_trueup():
         sb_renewal = data[contract]['Type']
         end_date = dparser.parse(data[contract]['Contract End Date']).date()
         start_date = dparser.parse(data[contract]['Contract Start Date']).date()
-        true_up_date = start_date - timedelta(days=30)
+        true_up_date = start_date - timedelta(days = 30)
 
         # True-Up Date (Feb 29 Fix)
         try:
@@ -115,7 +141,7 @@ def add_renewal_trueup():
         data[contract]['True Up Date'] = true_up_date.strftime('%Y-%m-%d')
 
         if sb_renewal == 'Renewal':
-            renewal_date = end_date + timedelta(days=1)
+            renewal_date = end_date + timedelta(days = 1)
             data[contract]['Renewal Date'] =renewal_date.strftime('%Y-%m-%d')
 
 def write_csv():
