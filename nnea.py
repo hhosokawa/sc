af03ce7878a1cc588669c7a8d60d1ac9bfc335a3
hhosokawa <hiro.hosokawa@gmail.com>
2014-03-12 13:41:21 -0400

    fix: region flag

--- nnea.py
+++ nnea.py
@@ -11,7 +11,7 @@ divregion =     csv_dic('auxiliary\\div-region.csv')
 divdistrict =   csv_dic('auxiliary\\div-district.csv')
 
 current_time =  time.strftime("%Y-%m-%d")
-output =        'P:/_HHOS/Microsoft/NNEA Summary - %s.csv' % (current_time)
+output =        'P:/_HHOS/Microsoft/NNEA Summary - %s.xls' % (current_time)
 
 ############### utils ###############
 
@@ -31,15 +31,28 @@ def district(series):
     return divdistrict.get(divloc, '')
 
 def apply_new_fields():
+    nnea_df['Region'] = nnea_df.apply(region, axis = 1)
     nnea_df['OB / TSR'] = nnea_df.apply(ob_tsr, axis = 1)
-    nnea_df['Region'] = nnea_df.apply(district, axis = 1)
     nnea_df['District'] = nnea_df.apply(district, axis = 1)
+
+def write_xls():
+    writer = pd.ExcelWriter(output)
+    nnea_df.to_excel(writer, 'nnea_data', index = False)
+
+    # Pivot Table
+    nnea_pt = pd.pivot_table(nnea_df,
+                             values='Contract Number',
+                             rows=['Region', 'District'],
+                             cols=['OB / TSR'],
+                             aggfunc=lambda x: len(x.unique()))
+    nnea_pt.to_excel(writer, 'nnea_summary')
+    writer.save()
 
 ############### nnea_main() ###############
 
 if __name__ == '__main__':
     t0 = time.clock()
     apply_new_fields()
-    nnea_df.to_csv(output, index=False)
+    write_xls()
     t1 = time.clock()
     print 'nnea_main() complete. Duration:', t1-t0
