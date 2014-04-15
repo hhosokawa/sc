9e7f96f5c7a3f512df555145ceb4266e01ae1a02
hhosokawa <hiro.hosokawa@gmail.com>
2014-04-15 15:35:14 -0400

    add: 097 div, clean

--- mda_rev_table.py
+++ mda_rev_table.py
@@ -10,60 +10,57 @@ gls = csv_dic('i/mda_rev_table/gl.csv')
 categories = csv_dic('i/mda_rev_table/categories.csv')
 
 rows = []
-months =    range(1,13)
-divs =      ['"*"', '"100"', '"200"']
-years =     ['2014', '2013', '2012', '2011', '2010']
-div_book =  {'"200"' : 'US', '"100"' : 'Canada', '"*"' : 'Consolidated'}
-
-quarterperiod = {1: 'Q1', 2: 'Q1', 3: 'Q1',
-                 4: 'Q2', 5: 'Q2', 6: 'Q2',
-                 7: 'Q3', 8: 'Q3', 9: 'Q3',
-                 10:'Q4', 11:'Q4', 12:'Q4'}
+qtrs =      range(1,5)
+years =     range(2009, 2015)
+divs =      {'"200"' : 'US',
+             '"097"' : 'US',
+             '"100"' : 'Canada',
+             '"*"' : 'Consolidated'}
+job = '"[*,/200279,/200349,/200350,/200351]"'
 
 ############## utils ###############
 
 # Make Spreadsheet Server Formula
-def makeformula(year, month, acct, cat, div):
+def makeformula(year, qtr, acct, cat, div):
     return ('=-' +
             'GXL("A","","TRANSLATED="&"E"&";"&"CURRENCY="&' +
             '"USD"' + '&";"&"BOOK="&' +     # Currency
             '"SC Consol - US"' + '&";",' +  # Book
-            year + ',' +                    # Year
-            '"PER"' + ',' +                 # PER or LTD
-            str(month) + ',' +              # Period
+            str(year) + ',' +               # Year
+            '"QTR"' + ',' +                 # PER / LTD / QTR
+            str(qtr) + ',' +                # Period / Quarter
             div + ',' +                     # Div
             '"*"' + ',' +                   # Dept
             '"*"' + ',' +                   # Territory
             acct + ',' +                    # GL Acct
-            cat + ',"*")')                  # Category
+            cat + ',' +                     # Category
+            job + ')')                      # Job Number
 
 # Concat MS Agency Fee Formula
-def concat_ms_fee(year, month, cat, div, formula):
-    ms_fees_formula = str(makeformula(year,
-                          month, '"631000"', cat, div))
+def concat_ms_fee(year, qtr, cat, div, formula):
+    ms_fees_formula = str(makeformula(year, qtr, '"631000"', cat, div))
     formula = formula + ' + ' + ms_fees_formula[2:]
     return formula
 
 def generate_rows():
-    for acct, year, cat, month, div in product(gls, years, categories, months, divs):
+    for acct, year, cat, qtr, div in product(gls, years, categories,
+                                             qtrs, divs):
         desc = gls[acct]
-        div_desc = div_book[div]
+        div_desc = divs[div]
         cat_desc = categories[cat]
-        quarter = quarterperiod[month]
-        formula = str(makeformula(year, month, acct, cat, div))
+        formula = str(makeformula(year, qtr, acct, cat, div))
 
         # Net Sales Edit - (Revenue - MS Agency Fees)
         if desc == 'Net Sales':
-            formula = concat_ms_fee(year, month,  cat, div, formula)
+            formula = concat_ms_fee(year, qtr, cat, div, formula)
 
-        r = (acct, desc, year, month, quarter,
-             div_desc, cat_desc, formula)
+        r = (acct, desc, year, qtr, div_desc, cat_desc, formula)
         rows.append(r)
     print 'generate_rows() complete.'
 
 def write_csv():
-    headers = ['GL', 'GL Desc', 'Year', 'Month',
-               'Quarter', 'Division', 'Super Category', 'FM']
+    headers = ['GL', 'GL Desc', 'Year', 'Quarter',
+               'Division', 'Super Category', 'FM']
 
     with open(output, 'wb') as o1:
         o1w = csv.writer(o1)
