da7332c4c715545cb0dcbd503743e2e8e1eecdc8
unknown <hhos@SCC4909.softchoice.com>
2015-05-29 16:15:33 -0400

    galneryus;

--- sales_expense_report_detail.py
+++ sales_expense_report_detail.py
@@ -8,7 +8,7 @@ import time
 
 # io
 input_dir = 'i/sales_expense_report_detail/'       # Update
-output = 'P:/_HHOS/Sales Expense Reporting/data/oracle_sales_expense_detail.csv'
+output = 'o/oracle_sales_expense_detail.csv'
 rows = []
 
 # Pictionary
@@ -42,33 +42,53 @@ def scan_csv():
 # Clean Payables
 def clean_payables(r):
 
+    # Amount
     if ',' in r['Invoice Distribution Functional Amount']:
         r['Amount'] = float(r['Invoice Distribution Functional Amount'].replace(',',''))
     else:
         r['Amount'] = float(r['Invoice Distribution Functional Amount'])
+    # Department Description
     if r['Department'] in departments:
         r['Department Desc'] = departments[r['Department']][1]
     else:
-        r['Department Desc'] = ''        
-    pprint(r)
-    raw_input()
+        r['Department Desc'] = ''
+    # District / Region via Territory
+    _, r['District'], r['Region'] = territories[r['Territory']]
+    if r['Region'] == 'Corporate':
+        if r['Division'] == '100':
+            r['District'] = 'CA Corporate'
+            r['Region'] = 'Canada'
+        else:
+            r['District'] = 'US Corporate'
+            r['Region'] = 'US Corporate'    
+    # GL Parent
+    r['GL Parent'] = hierarchies[r['GL Account']][1]
+    # Period / Year
+    dt = parse(r['Period Name'])
+    r['Period'] = dt.month
+    r['Year'] = dt.year
+    if r['Amount'] != 0:
+        rows.append(r)
     
 # Clean Oracle - Sales, Travel, Meal expenses
 def clean_oracle(r, year, period, book, currency, actual_plan):
 
     if r['Category_'] != 'Purchase Invoices':
         r.pop('', None)
+        
+        # Amount
         if ',' in r['Base Credit']: 
             r['Base Credit'] = r['Base Credit'].replace(',', '')
         if ',' in r['Base Debit']: 
             r['Base Debit'] = r['Base Debit'].replace(',', '')
         r['Amount'] = float(r['Base Debit']) - float(r['Base Credit'])
-        _, r['District'], r['Region'] = territories[r['Territory']]
+        # Department Description
         if r['Department'] in departments:
             r['Department Desc'] = departments[r['Department']][1]
         else:
             r['Department Desc'] = ''
-        r['GL Parent'] = hierarchies[r['GL Account']][1]
+        # District / Region via Territory
+        _, r['District'], r['Region'] = territories[r['Territory']]
         if r['Region'] == 'Corporate':
             if r['Division'] == '100':
                 r['District'] = 'CA Corporate'
@@ -76,20 +96,23 @@ def clean_oracle(r, year, period, book, currency, actual_plan):
             else:
                 r['District'] = 'US Corporate'
                 r['Region'] = 'US Corporate'
+        # GL Parent
+        r['GL Parent'] = hierarchies[r['GL Account']][1]
+        # Supplier name via Line Description
         if '.' in r['Line Description']:
             r['Line Description'] = r['Line Description'].replace('.', '')
-        r['Supplier Name'] = employee_names.get(r['Line Description'],'')
+        r['Supplier Name'] = employee_names.get(r['Line Description'], r['Line Description'])
         if r['Amount'] != 0:
             rows.append(r)
 
 ############### Data Output ###############
 def write_csv():
     # Oracle - Sales Expense Report
-    headers = ['Actual/Plan', 'Amount', 'Book', 'Currency', 'Category A',
-               'Category B', 'Category C', 'Category D', 'Description',
-               'Discretionary Expense', 'District', 'GL Account', 
-               'GL GrandParent', 'GL Parent', 'OB or TSR', 'Period', 'Quarter', 
-               'Region', 'Solution Group', 'Territory', 'Year']
+    headers = ['Amount', 'Category', 'Category_', 'Department', 
+               'Department Desc', 'Distribution Line Type Code', 'District', 
+               'Division', 'GL Account', 'GL Parent', 'Invoice Description', 
+               'Line Description', 'Period', 'Region', 'Source_', 
+               'Supplier Name', 'Territory', 'Year']
 
     with open(output, 'wb') as o0:
         o0w = csv.DictWriter(o0, delimiter=',',
@@ -97,38 +120,12 @@ def write_csv():
         o0w.writerow(dict((fn, fn) for fn in headers))
         for r in rows:
             o0w.writerow(r)
-    
-    # Discoverer - Supplier Name Report
-    headers = ['Cat', 'Creation Date', 'Department', 'Division', 'GL Account',
-               'GL Description', 'GL Parent', 'Invoice Date', 'Invoice Number',
-               'Line Amount SUM', 'Period Name', 'Project', 'Region', 'Supplier Name',
-               'Territory']
-
-    with open(output1, 'wb') as o1:
-        o1w = csv.DictWriter(o1, delimiter=',',
-                             fieldnames=headers, extrasaction='ignore')
-        o1w.writerow(dict((fn, fn) for fn in headers))
-        for r in supplier_name_rows:
-            o1w.writerow(r)
-            
-    # Discoverer - Employee Audit Expense Report + Headcount info
-    headers = ['District', 'Employee Name', 'Employee Number', 
-               'Functional Group',  'Invoice Num', 'Overrider Approver Name',
-               'Receipts Received Date', 'Region', 'Report Submitted Date']    
-
-    with open(output2, 'wb') as o2:
-        o2w = csv.DictWriter(o2, delimiter=',',
-                             fieldnames=headers, extrasaction='ignore')
-        o2w.writerow(dict((fn, fn) for fn in headers))
-        for id in headcount_dict:
-            o2w.writerow(headcount_dict[id])
     print 'write_csv() complete.'
              
 ############### main ###############
 if __name__ == '__main__':
     t0 = time.clock()
     scan_csv()
-    rows = create_hierarchy()
     write_csv()
     t1 = time.clock()
-    print 'sales_expense_report.py complete.', t1-t0
+    print 'sales_expense_report_detail.py complete.', t1-t0
