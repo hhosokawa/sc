a2575717105ae0c33612704395a8add1ac0bf45b
unknown <hhos@SCC4909.softchoice.com>
2015-08-20 11:30:34 -0400

    add new vendor header to vendor_yield.py

--- vendor_yield.py
+++ vendor_yield.py
@@ -14,7 +14,28 @@ categories = csv_dic('i/vendor_yield/hierarchies/categories.csv')
 gl_parent = csv_dic('i/vendor_yield/hierarchies/gl_parent.csv')
 vendors = csv_dic('i/vendor_yield/hierarchies/vendors.csv')
 
+# Vendor Lists
+top_vendors = ['MICROSOFT', 'CISCO SYSTEMS', 'DELL COMPUTER',
+               'VMWARE', 'LENOVO', 'IBM', 'ADOBE SYSTEMS',
+               'NETAPP', 'SYMANTEC', 'HP', 'EMC CORPORATION',
+               'MCAFEE', 'SOPHOS', 'CITRIX SYSTEMS', 'APPLE',
+               'RED HAT SOFTWARE', 'TREND MICRO']
+HP_fix = ['HP INC', 'HEWLETT-PACKARD ENTERPRISE', 'HEWLETT PACKARD']
+
 ############### utils ###############
+# Include / Edit Top Vendors
+def vendor_name_fix(managed_vendor_name):
+    managed_vendor_name_top = ''
+    # Consolidated HP
+    if managed_vendor_name in HP_fix:
+        managed_vendor_name_top = 'HP'
+    # Only include vendors from top_vendors list
+    elif managed_vendor_name in top_vendors:
+        managed_vendor_name_top = managed_vendor_name
+    else:
+        managed_vendor_name_top = 'All Other'
+    return managed_vendor_name_top 
+
 # BI
 def scan_bi(r):
     # Adjust BI Super Categories
@@ -26,7 +47,8 @@ def scan_bi(r):
     if (r['Solution Group'] == 'PRODUCT' 
         and r['Solution Type'] == 'SaaS - Cloud'):
         r['Super Category'] = 'SaaS'
-
+    r['Managed Vendor Name Top'] = vendor_name_fix(r['Managed Vendor Name'])
+    
     r['Amount'] = r['USD GP']
     r['GL Parent'] = 'FM'
     rows.append(r.copy())
@@ -47,6 +69,7 @@ def scan_oracle(r, year, qtr):
         r['Fiscal Quarter'] = int(qtr)
         r['GL Parent'] = gl_parent.get(r['GL Account'],'')
         r['Managed Vendor Name'] = vendors.get(r['Vendor'], 'All Other')
+        r['Managed Vendor Name Top'] = vendor_name_fix(r['Managed Vendor Name'])
         r['Super Category'] = categories.get(r['Category'],'')
         
         # 2014 Cisco Super Category
@@ -98,9 +121,9 @@ def create_zeros():
 def write_csv():
     headers = ['Amount', 'Category', 'Calendar Year', 'Department', 
                'Description', 'Division', 'Fiscal Quarter', 'GL Account', 
-               'GL Parent', 'Managed Vendor Name', 'SCC Category', 
-               'Solution Group', 'Solution Type', 'Super Category', 
-               'Territory']
+               'GL Parent', 'Managed Vendor Name', 'Managed Vendor Name Top',
+               'SCC Category', 'Solution Group', 'Solution Type', 
+               'Super Category', 'Territory']
     with open(output, 'wb') as o0:
         o0w = csv.DictWriter(o0, delimiter=',',
                              fieldnames=headers, extrasaction='ignore')
